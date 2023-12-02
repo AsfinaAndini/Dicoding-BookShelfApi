aa3a2b80447243fd550ebc92c71335f30911a16c
AsfinaAndini <128123181+AsfinaAndini@users.noreply.github.com>
2023-12-02 15:04:52 +0700

    get item by id

--- src/handler.js
+++ src/handler.js
@@ -146,9 +146,31 @@ const getAllItemsHandler = (request, h) => {
             },
         })
         .code(200); return response;
-}
+};
 
-const getItemByIdHandler = (request, h) => { }
+const getItemByIdHandler = (request, h) => {
+    const { bookId } = request.params;
+    const book = items.filter((note) => note.id === bookId)[0];
+    // Jika id buku yang dicari ditemukan
+    if (book) {
+        const response = h
+            .response({
+                status: 'success',
+                data: {
+                    book,
+                },
+            })
+            .code(200);
+        return response;
+    }// Jika id buku yang dicari tidak ditemukan
+    const response = h
+        .response({
+            status: 'fail',
+            message: 'Buku tidak ditemukan',
+        })
+        .code(404);
+    return response;
+};
 
 const editItemByIdHandler = (request, h) => { }
 
